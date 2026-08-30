bf5341b3bef1c2e05d28d3dc1496e84ade75af7c
IsabelaPiedrahita <isa.piedrahita.russi@gmail.com>
2026-08-30 17:51:32 -0500

    Clean up comments in CargoHold.java
    Removed unnecessary comments from CargoHold methods.

--- labw02A/src/cargohold/CargoHold.java
+++ labw02A/src/cargohold/CargoHold.java
@@ -13,9 +13,7 @@ public class CargoHold {
 
     int[] cargoUnits = {42, 17, 88, 5, 63, 24, 71, 9};
 
-    /**
-     * Returns the total number of units across every supply type.
-     */
+
     public int getTotalUnits() {
         int total = 0;
         for (int i = 0; i < cargoUnits.length; i++) {
@@ -24,9 +22,6 @@ public class CargoHold {
         return total;
     }
 
-    /**
-     * Returns the amount of the most heavily stocked supply type.
-     */
     public int getMostStockedAmount() {
         int max = cargoUnits[0];
         for (int i = 1; i < cargoUnits.length; i++) {
@@ -37,9 +32,6 @@ public class CargoHold {
         return max;
     }
 
-    /**
-     * Returns the amount of the least stocked supply type.
-     */
     public int getLeastStockedAmount() {
         int min = cargoUnits[0];
         for (int i = 1; i < cargoUnits.length; i++) {
@@ -50,18 +42,10 @@ public class CargoHold {
         return min;
     }
 
-    /**
-     * Returns the average number of units per supply type, as a
-     * double so the result can include a decimal portion.
-     */
     public double getAverageUnits() {
         return (double) getTotalUnits() / cargoUnits.length;
     }
 
-    /**
-     * Prints a short readable report of this cargo hold using the
-     * methods above.
-     */
     public void printCargoReport() {
         System.out.println("Total units aboard: " + getTotalUnits());
         System.out.println("Most stocked amount: " + getMostStockedAmount());
